527741fd53df8ab2f373d70a193d0c82246f877c
Bernd Schuller <b.schuller@fz-juelich.de>
2022-05-23 14:12:08 +0200

    cleanup dependencies

--- pdp/src/main/java/eu/unicore/uas/pdp/argus/ArgusPAPClient.java
+++ pdp/src/main/java/eu/unicore/uas/pdp/argus/ArgusPAPClient.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import eu.unicore.services.ws.WSRFClientFactory;
+import eu.unicore.security.wsutil.client.WSClientFactory;
 import eu.unicore.util.httpclient.IClientConfiguration;
 import xmlbeans.oasis.xacml.x2.x0.saml.assertion.XACMLPolicyQueryDocument;
 import xmlbeans.org.oasis.saml2.protocol.ResponseDocument;
@@ -18,7 +18,7 @@ public class ArgusPAPClient{
 		{
 			sec.getHttpClientProperties().setConnectionTimeout(timeout);
 			sec.getHttpClientProperties().setSocketTimeout(timeout);
-			WSRFClientFactory factory = new WSRFClientFactory(sec);
+			WSClientFactory factory = new WSClientFactory(sec);
 			samlProxy = factory.createPlainWSProxy(SAMLXACMLProvisioningInterface.class, url);
 		}catch(MalformedURLException m)
 		{
