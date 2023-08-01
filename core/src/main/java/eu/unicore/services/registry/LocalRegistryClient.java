c42e59f377698ed72dbbc90f04b11e540356127f
Bernd Schuller <b.schuller@fz-juelich.de>
2023-08-01 14:19:42 +0200

    cache the list of registry entries

--- core/src/main/java/eu/unicore/services/registry/LocalRegistryClient.java
+++ core/src/main/java/eu/unicore/services/registry/LocalRegistryClient.java
@@ -2,6 +2,7 @@ package eu.unicore.services.registry;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -37,16 +38,26 @@ public class LocalRegistryClient implements IRegistry {
 		try (RegistryImpl reg = (RegistryImpl)getHome().getForUpdate(resID)){
 			return reg.addEntry(endpoint, content, requestedTT);
 		}
+		finally {
+			invalidateCache();
+		}
 	}
 	
 	public void refresh(String endpoint, Map<String,String>content) throws Exception {
 		try (RegistryImpl reg = (RegistryImpl)getHome().getForUpdate(resID)){
 			reg.refresh(endpoint, content);
 		}
+		finally {
+			invalidateCache();
+		}
 	}
 	
 	public List<Map<String,String>> listEntries() throws Exception {
-		List<Map<String,String>> res = new ArrayList<>();
+		List<Map<String,String>> res = getCached();
+		if(res!=null) {
+			return res;
+		}
+		res = new ArrayList<>();
 		RegistryImpl reg = null;
 		if(kernel.getMessaging().hasMessages(resID)){
 			reg = (RegistryImpl)getHome().refresh(resID);
@@ -57,6 +68,7 @@ public class LocalRegistryClient implements IRegistry {
 		for(String entryID: reg.getModel().getEntryIDs()){
 			res.add(getEntry(entryID).getModel().getContent());
 		}
+		cache(res);
 		return res;
 	}
 
@@ -73,4 +85,28 @@ public class LocalRegistryClient implements IRegistry {
 		if(home==null)throw new IllegalStateException("No Registry service deployed");
 		return home;
 	}
+
+
+	private synchronized List<Map<String,String>> getCached(){
+		Long upd = updated.get(resID);
+		if(upd!=null && System.currentTimeMillis()<upd+cacheTime) {
+			return cache.get(resID); 
+		}
+		return null;
+	}
+	
+	private synchronized void cache(List<Map<String,String>> entries) {
+		cache.put(resID, entries);
+		updated.put(resID, System.currentTimeMillis());
+	}
+	
+	private synchronized void invalidateCache() {
+		cache.remove(resID);
+		updated.remove(resID);
+	}
+
+	private static final long cacheTime = 3600 * 1000;
+	private static final Map<String,List<Map<String,String>>>cache = new HashMap<>();
+	private static final Map<String,Long>updated= new HashMap<>();
+
 }
